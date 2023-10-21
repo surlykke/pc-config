c0021211f9131c16d03aab04bf28e19152b94810
Christian Surlykke <christian@surlykke.dk>
2023-10-21 08:55:45 +0200

    Assume Sway as WM

--- home/.local/bin/work.sh
+++ home/.local/bin/work.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 thisdir=$(realpath $(dirname $0))
 echo $thisdir
-ToSkærme.sh
-i3-msg "workspace 1; append_layout $thisdir/arb_ws_1.json"
-i3-msg "workspace 2; append_layout $thisdir/arb_ws_2.json"
-sleep 1
 nohup alacritty --working-directory $HOME -T VPN >/dev/null 2>/dev/null &
 nohup alacritty --working-directory $HOME/projekter/payroll -T Work >/dev/null 2>/dev/null &
 nohup alacritty --working-directory $HOME/projekter/payroll -T Log >/dev/null 2>/dev/null &
@@ -15,4 +11,4 @@ nohup intellij-idea-ultimate nosplash  >/dev/null 2>/dev/null &
 nohup slack >/dev/null 2>/dev/null &
 nohup chromium --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo >/dev/null 2>/dev/null &
 
-i3-msg '[title="^VPN$"] focus'
+swaymsg '[title="^VPN$"] focus'
