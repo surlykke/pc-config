c98c0fba4867663dfbe069d3eaad9dbcae1bcffb
Christian Surlykke <christian@surlykke.dk>
2023-10-22 11:18:23 +0200

    Look for BROWSER and MONITOR env variables when copying

--- home/.local/bin/browsersetup.sh
+++ home/.local/bin/browsersetup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-nohup chromium https://outlook.office.com/mail > /dev/null 2>/dev/null &
+nohup $BROWSER https://outlook.office.com/mail > /dev/null 2>/dev/null &
 sleep 1
-chromium https://outlook.office.com/calendar/view/week
-chromium https://paychexeurope.atlassian.net/jira/software/c/projects/DL/boards/147
-chromium https://jenkins.lessor.dk/view/Payroll
-chromium mail.google.com/mail/u/0/#chat/dm/n3GWhAAAAAE
+$BROWSER https://outlook.office.com/calendar/view/week
+$BROWSER https://paychexeurope.atlassian.net/jira/software/c/projects/DL/boards/147
+$BROWSER https://jenkins.lessor.dk/view/Payroll
+$BROWSER mail.google.com/mail/u/0/#chat/dm/n3GWhAAAAAE
 
--- homeConfig.sh
+++ homeConfig.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/bash
 [[ "root" == "$(whoami)" ]] && { echo This script should not be run as root but as thy self; exit 1; }
 
+# Try to figure out some env 
+if [[ -z "$BROWSER" ]]; then
+	for browser in chromium google-chrome brave-browser brave; do 
+		if type $browser >/dev/null 2>/dev/null; then
+			export BROWSER=$browser
+			break
+		fi
+	done
+	
+
+	if [[ -z "$BROWSER" ]]; then
+		echo "Env var BROWSER not set and no suitable browser found in path" >&2
+		exit 1
+	fi
+fi	
+
+if [[ -z "$MONITOR" ]]; then
+	echo "Env var MONITOR not set" >&2  # FIXME Try to figure out
+	exit 1
+fi
+
 cd $(dirname $0)
 
 echo "Copying files under $HOME"
 
 # Set up for envsubst
-export MONITOR=$(xrandr | grep 'connected primary' | cut -d ' ' -f 1)
-SHELL_FORMAT='$USER $MONITOR'
+SHELL_FORMAT='$USER $MONITOR $BROWSER'
 
 for dir in $(find home -type d -printf "%P\n"); do 
 	mkdir -p ${HOME}/$dir
