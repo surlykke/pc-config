42c0afb7f897f04b2d7e63b8f738c7625d717a26
Christian Surlykke <christian@surlykke.dk>
2024-06-29 15:15:20 +0200

    nvim: add oil plugin

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -555,6 +555,23 @@ require("lazy").setup({
 	{
 		"ThePrimeagen/vim-be-good",
 	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = {
+					"icon",
+					"permissions",
+					"size",
+					"mtime",
+				},
+			})
+			vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
