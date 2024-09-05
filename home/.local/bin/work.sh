3bbd9a7725b08beeb341ee472b2673c20935f4c4
Christian Surlykke <christian@surlykke.dk>
2024-09-05 22:50:41 +0200

    Ditch foot. Back to alacritty

--- home/.local/bin/work.sh
+++ home/.local/bin/work.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 thisdir=$(realpath $(dirname $0))
 echo $thisdir
-nohup foot --working-directory=$HOME --override=locked-title=yes -T VPN >/dev/null 2>/dev/null &
-nohup foot --working-directory=$HOME/projekter/payroll --override=locked-title=yes -T Work >/dev/null 2>/dev/null &
-nohup foot --working-directory=$HOME/projekter/payroll --override=locked-title=yes -T Log >/dev/null 2>/dev/null &
+nohup alacritty --working-directory $HOME -T VPN >/dev/null 2>/dev/null &
+nohup alacritty --working-directory $HOME/projekter/payroll -T Work >/dev/null 2>/dev/null &
+nohup alacritty --working-directory $HOME/projekter/payroll -T Log >/dev/null 2>/dev/null &
 
 . $thisdir/browsersetup.sh
 nohup intellij-idea-ultimate nosplash  >/dev/null 2>/dev/null &
