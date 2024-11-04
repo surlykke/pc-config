55cf10fa35b98c82ad93a66c96c5c40f6969abca
Christian Surlykke <christian@surlykke.dk>
2024-11-04 18:14:37 +0100

    Remove vpn terminal from work setup

--- home/.local/bin/work.sh
+++ home/.local/bin/work.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 thisdir=$(realpath $(dirname $0))
 echo $thisdir
-nohup alacritty --working-directory $HOME -T VPN >/dev/null 2>/dev/null &
 nohup alacritty --working-directory $HOME/projekter/payroll -T Work >/dev/null 2>/dev/null &
 nohup alacritty --working-directory $HOME/projekter/payroll -T Log >/dev/null 2>/dev/null &
 
