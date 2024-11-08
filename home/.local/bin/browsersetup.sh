3cc9825081f19a1ded82899f270bff2e352d8ce6
Christian Surlykke <christian@surlykke.dk>
2024-11-08 08:42:45 +0100

    Browsersetup: Change url of jira page

--- home/.local/bin/browsersetup.sh
+++ home/.local/bin/browsersetup.sh
@@ -2,7 +2,7 @@
 nohup google-chrome https://outlook.office.com/mail > /dev/null 2>/dev/null &
 sleep 1
 google-chrome https://outlook.office.com/calendar/view/week
-google-chrome https://paychexeurope.atlassian.net/jira/software/c/projects/DL/boards/147
+google-chrome https://paychexeurope.atlassian.net/jira/software/c/projects/DL/boards/1256
 google-chrome https://jenkins.lessor.dk/view/Payroll
 google-chrome mail.google.com/mail/u/0/#chat/dm/n3GWhAAAAAE
 
