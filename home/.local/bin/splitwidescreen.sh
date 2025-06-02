708f65ae7ca18e3f4efcb46935c1ab938609b198
Christian Surlykke <christian@surlykke.dk>
2025-06-02 20:49:20 +0200

    clean up

--- home/.local/bin/splitwidescreen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-xrandr --setmonitor MON-1 2560/600x1440/340+1920+0 DP1
-xrandr --setmonitor MON-2 2560/600x1440/340+4480+0 none
-
--- home/.local/bin/unsplitwidescreen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-xrandr --delmonitor MON-1
-xrandr --delmonitor MON-2
-
